301889d086b6b8068a03d2adf1a5c359621ea7e9
Ozioma45 <oziomaegole@gmail.com>
2025-04-29 21:36:19 +0100

    added the deployed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/auth",
+  baseURL: "https://urgent-2kay-directed-bill-payment-system.onrender.com/auth",
+  //baseURL: "http://localhost:5000/auth",
   withCredentials: true,
 });
